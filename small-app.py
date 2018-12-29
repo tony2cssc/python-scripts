f249dc16581d2369530f849eefd448ede5629e7c
Tony Guo <jijing.guo@gmail.com>
2018-12-29 21:16:15 +0800

    changed to get the file name automatically.

--- small-app.py
+++ small-app.py
@@ -1,24 +1,32 @@
 import os
+from builtins import len
 
-import os
+import sys
+#
+# if sys.argv[1]:
+#     path = sys.argv[1]
+#     print("path: " + path)
+# else:
 
-for root, dirs, files in os.walk("E:\BaiduNetdiskDownload\海底小纵队英文版93集\S2季23集（带字幕）"):
-    for file in files:
-        print(file)
-        if file.__contains__("S02E"):
-            new_file
-        for i in range(1, 10):
-            cmd = 'D:/Tools/mkvtoolnix\mkvmerge.exe --ui-language zh_CN --output ' \
-                  '^"E:\BaiduNetdiskDownload\海底小纵队英文版93集\S2季23集（带字幕）\S02E0%(id)d.mkv^" --language 0:und ^"^(^" ' \
-                  '^"E:\BaiduNetdiskDownload\海底小纵队英文版93集\S2季23集（带字幕）\Octonauts_S02E0%(id)d_*.srt^" ' \
-                  '^"^)^" --language 0:und --language 1:und ^"^(^" ^"E:\BaiduNetdiskDownload\海底小纵队英文版93集\S2季23集' \
-                  '（带字幕）\Octonauts_S02E0%(id)d_*.avi^" ^"^)^" --track-order 0:0,1:0,1:1' % {"id": i}
-            os.system(cmd)
+path = 'E:\\BaiduNetdiskDownload\\海底小纵队英文版93集\\S3季18集（带字幕）'
 
-        for j in range(10, 23):
-            cmd = 'D:/Tools/mkvtoolnix\mkvmerge.exe --ui-language zh_CN --output ' \
-                  '^"E:\BaiduNetdiskDownload\海底小纵队英文版93集\S2季23集（带字幕）\S02E0%(id)d.mkv^" --language 0:und ^"^(^" ' \
-                  '^"E:\BaiduNetdiskDownload\海底小纵队英文版93集\S2季23集（带字幕）\Octonauts_S02E0%(id)d_The_Colossal_Squid.srt^" ' \
-                  '^"^)^" --language 0:und --language 1:und ^"^(^" ^"E:\BaiduNetdiskDownload\海底小纵队英文版93集\S2季23集' \
-                  '（带字幕）\Octonauts_S02E0%(id)d_The_Colossal_Squid.avi^" ^"^)^" --track-order 0:0,1:0,1:1' % {"id": j}
-            os.system(cmd)
+for root, dirs, files in os.walk(path):
+    files.sort()
+    i = 0
+    k = 1
+    while i < len(files):
+        if files[i].__contains__('Octonauts_'):
+            new_filename = files[i][len('Octonauts_'):-4]
+        else:
+            new_filename = files[i][:-4]
+
+        # print(new_filename)
+
+        cmd = 'D:/Tools/mkvtoolnix\mkvmerge.exe --ui-language zh_CN --output ' \
+              '^"%(root)s\%(new)s.mkv^" --language 0:und ^"^(^" ^"%(root)s\%(file2)s^" ' \
+              '^"^)^" --language 0:und --language 1:und ^"^(^" ^"%(root)s\%(file1)s^" ^"^)^" --track-order 0:0,' \
+              '1:0,1:1' % {"root": root, "new": new_filename, "file1": files[i], "file2": files[i+1], "id": i}
+
+        # print(cmd)
+        os.system(cmd)
+        i = i + 2
