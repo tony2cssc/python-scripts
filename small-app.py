fb516f362858c13773a14ba4409b6a1dd47a2545
Tony Guo <jijing.guo@gmail.com>
2019-01-03 00:05:21 +0800

    optimize the logic of finding the files

--- small-app.py
+++ small-app.py
@@ -1,3 +1,4 @@
+import glob
 import os
 from builtins import len
 
@@ -8,25 +9,23 @@ import sys
 #     print("path: " + path)
 # else:
 
-path = 'E:\\BaiduNetdiskDownload\\海底小纵队英文版93集\\S3季18集（带字幕）'
+path = 'E:/BaiduNetdiskDownload/海底小纵队英文版93集/S3季18集（带字幕）'
 
-for root, dirs, files in os.walk(path):
-    files.sort()
-    i = 0
-    k = 1
-    while i < len(files):
-        if files[i].__contains__('Octonauts_'):
-            new_filename = files[i][len('Octonauts_'):-4]
-        else:
-            new_filename = files[i][:-4]
+os.chdir(path)
 
-        # print(new_filename)
+for f in glob.glob('*.mp4'):
+    (short_name, extension) = os.path.splitext(f)
+    # print(short_name, extension)
+    if short_name.__contains__('Octonauts_'):
+        new_filename = short_name[len('Octonauts_'):]
 
-        cmd = 'D:/Tools/mkvtoolnix\mkvmerge.exe --ui-language zh_CN --output ' \
-              '^"%(root)s\%(new)s.mkv^" --language 0:und ^"^(^" ^"%(root)s\%(file2)s^" ' \
-              '^"^)^" --language 0:und --language 1:und ^"^(^" ^"%(root)s\%(file1)s^" ^"^)^" --track-order 0:0,' \
-              '1:0,1:1' % {"root": root, "new": new_filename, "file1": files[i], "file2": files[i+1], "id": i}
+    cmd = 'D:/Tools/mkvtoolnix\mkvmerge.exe --ui-language zh_CN --output ' \
+          '^"%(root)s/%(new)s.mkv^" --language 0:und ^"^(^" ^"%(root)s/%(file2)s.srt^" ' \
+          '^"^)^" --language 0:und --language 1:und ^"^(^" ^"%(root)s/%(file1)s.mp4^" ^"^)^" --track-order 0:0,' \
+          '1:0,1:1' % {"root": path, "new": new_filename, "file1": short_name, "file2": short_name}
 
-        # print(cmd)
-        os.system(cmd)
-        i = i + 2
+
+
+    # print(cmd)
+    os.system(cmd)
+
